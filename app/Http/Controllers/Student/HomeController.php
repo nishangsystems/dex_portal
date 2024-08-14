d013dcbb03fd2d6d3ae63deeccd177d5c3f716d6
germanusK <germanuskeming@gmail.com>
2024-08-14 09:23:31 +0100

    -----

--- app/Http/Controllers/Student/HomeController.php
+++ app/Http/Controllers/Student/HomeController.php
@@ -209,8 +209,10 @@ class HomeController extends Controller
                 $application = new ApplicationForm();
                 $application->student_id = auth('student')->id();
                 $application->year_id = Helpers::instance()->getCurrentAccademicYear();
+                $application->id = 5;
                 $application->save();
             }
+            $application->update(['campus_id'=>5]);
             if($application->degree_id != null and ($application->tranzak_transaction == null || $application->tranzak_transaction->payment_id != $application->degree_id) and $step != 0 ){
                 $data['step'] = 6;
             }elseif($application->degree_id != null and ($application->tranzak_transaction != null and $application->tranzak_transaction->payment_id == $application->degree_id) and $step == 6){
