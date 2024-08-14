d1f070f50a5cd402ab811763ed932cc2f921cea9
germanusK <germanuskeming@gmail.com>
2024-08-14 09:15:21 +0100

    -----

--- app/Http/Controllers/Student/HomeController.php
+++ app/Http/Controllers/Student/HomeController.php
@@ -219,8 +219,8 @@ class HomeController extends Controller
             $data['certificates'] = collect(json_decode($this->api_service->certificates())->data);
             $data['application'] = $application;
             if($application->entry_qualification != null){
-                dd(\App\Models\TranzakCredential::all());
-                dd($application);
+                // dd(\App\Models\TranzakCredential::all());
+                // dd($application);
                 $data['programs'] = collect(json_decode($this->api_service->campusDegreeCertificatePrograms($application->campus_id, $application->degree_id, $application->entry_qualification))->data);
             }
             $data['aux_programs'] = \App\Models\Program::where('type', 'auxiliary')->get();
