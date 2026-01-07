8a5733876ae75e9d976a61d7a4de2a306fb4b5a8
germanusk <germanuskeming@gmail.com>
2026-01-07 11:28:54 +0100

    ------

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Language::class,
-            \App\Http\Middleware\ServiceBlocker::class,
+            // \App\Http\Middleware\ServiceBlocker::class,
         ],
         
         'api' => [
