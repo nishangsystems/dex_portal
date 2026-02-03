43b0c2951fb74865b2e3e26b578d6dc6af01f2ca
germanusk <germanuskeming@gmail.com>
2026-02-03 17:36:04 +0100

    -----

--- app/Http/Controllers/Admin/ProgramController.php
+++ app/Http/Controllers/Admin/ProgramController.php
@@ -1111,7 +1111,11 @@ class ProgramController extends Controller
             $data['title'] = "Admit Student";
             $data['_this'] = $this;
             $data['action'] = __('text.word_admit');
-            $data['applications'] = ApplicationForm::whereNotNull('transaction_id')->whereNotNull('submitted')->whereNull('admitted')->where('year_id', Helpers::instance()->getCurrentAccademicYear())->get();
+            $data['applications'] = ApplicationForm::
+                whereNotNull('transaction_id')
+                // ->whereNotNull('submitted')
+                ->whereNull('admitted')
+                ->where('year_id', Helpers::instance()->getCurrentAccademicYear())->get();
             $data['programs'] = collect(json_decode($this->api_service->programs())->data);
             return view('admin.student.applications', $data);
         }
